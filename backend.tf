1ea8c1b6906823e910ef3d9e2f9e96256c556c11
LH <liban240@gmail.com>
2023-09-19 11:42:39 +0100

    fix typo

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "hodt-backend"
+    bucket         = "hotd-backend"
     key            = "terraform.tfstate"
     region         = "eu-west-2"
   }
