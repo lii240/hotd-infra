b9ce245ee292df58c106f692a211dd440bcbe60c
LH <liban240@gmail.com>
2023-09-19 12:43:05 +0100

    update bucket policy

--- s3.tf
+++ s3.tf
@@ -32,6 +32,7 @@ data "aws_iam_policy_document" "hotd_allow_public_access" {
 
     actions = [
       "s3:GetObject",
+      "s3:ListBucket",
     ]
 
     resources = [aws_s3_bucket.hotd.arn, "${aws_s3_bucket.hotd.arn}/*",]
